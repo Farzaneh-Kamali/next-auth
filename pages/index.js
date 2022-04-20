2f0c28e94ccde750bb9c15544ee118f43a621c87
farzaneh <farzaneh.kamali@gmail.com>
2022-04-20 14:34:54 +0430

    Build project

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { useSession } from 'next-auth/react';
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  console.log(session, status);
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +16,8 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {session ? `${session.user.name}` : ''} Welcome to{' '}
+          <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
@@ -65,5 +69,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
